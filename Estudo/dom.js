7a7408d3b4d7e410901d424d44ecd0928cd45ea1
andersondev3050 <andersoncubano@hotmail.com>
2024-08-16 13:37:21 -0300

    git commit-modified

---
+++ Estudo/dom.js
@@ -0,0 +1,7 @@
+const parágrafo1=document.getElementById("parágrafo1");
+const parágrafo2=document.getElementById("parágrafo2");
+const parágrafo3=document.getElementById("parágra3");
+
+const arrayElements=[parágrafo1,parágrafo2,parágrafo3];
+
+console.log("arrayElements");
