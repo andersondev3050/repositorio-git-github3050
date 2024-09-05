d1c7d37a5424afb6b058fe1d4fec999e57a204c6
andersondev3050 <andersoncubano@hotmail.com>
2024-09-05 12:22:52 -0300

    git commit -m "alterando DOM"

--- Estudo/dom.js
+++ Estudo/dom.js
@@ -1,7 +1,25 @@
-const parágrafo1=document.getElementById("parágrafo1");
-const parágrafo2=document.getElementById("parágrafo2");
-const parágrafo3=document.getElementById("parágra3");
+// Criando Arrays
+const dc1 = document.getElementById('c1');
+const dc2 = document.getElementById('c2');
+const dc3 = document.getElementById('c3');
+const dc4 = document.getElementById('c4');
+const dc5 = document.getElementById('c5');
+const dc6 = document.getElementById('c6');
 
-const arrayElements=[parágrafo1,parágrafo2,parágrafo3];
+const arrrayElementos = [dc1, dc2, dc3, dc4, dc5,dc6];
+// Utilizando sprashing el element  que se  est e
+const colecaoHTML = [...document.getElementsByTagName('div')]
 
-console.log("arrayElements");
+
+// Adicionando eventos aos elementos do array
+//for(d of arrrayElementos)
+    //d.innerHTML = 'AndersonDev'
+
+console.log(colecaoHTML)
+colecaoHTML.map((e)=>{
+    console.log(e)
+})
+
+
+
+
